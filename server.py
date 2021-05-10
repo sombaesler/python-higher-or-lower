36d1caf5b02ce6b79330d2155baeb7d8792529f2
cpotipat <namsom.chitchanok@gmail.com>
2021-05-10 12:50:17 -0700

    Style HTML by using Advanced Decorators function with *args and **kwargs

--- server.py
+++ server.py
@@ -6,7 +6,41 @@ app = Flask(__name__)
 random_number = random.randint(0, 9)
 
 
+def make_emphasis(function):
+    def wrapper():
+        return f"<em>{function()}</em>"
+
+    return wrapper
+
+
+def decorator(function):
+    def text_wrapper(*args, **kwargs):
+        return f"<h1 style='text-align: center; color: yellow; background-color: black; width: 300px'>You guessed " \
+               f"{args[0]}:</h1> {function(args[0])}"
+
+    return text_wrapper
+
+
+@decorator
+def too_low(number):
+    return "<h2 style='color: purple'>Too high, try again!</h2>" \
+           "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'/>"
+
+
+@decorator
+def too_high(number):
+    return "<h2 style='color: red'>Too low, try again!</h2>" \
+           "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'/>"
+
+
+@decorator
+def correct_number(number):
+    return "<h2 style='color: green'>You found me!</h2>" \
+           "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'/>"
+
+
 @app.route("/")
+@make_emphasis
 def greet():
     return '<h1>Guess a number between 0 and 9</h1>' \
            '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif">'
@@ -15,15 +49,13 @@ def greet():
 @app.route("/<int:guess>")
 def compare(guess):
     if guess > random_number:
-        return "<h1 style='color: purple'>Too high, try again!</h1>" \
-               "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'/>"
+        return too_low(guess)
 
     elif guess < random_number:
-        return "<h1 style='color: red'>Too low, try again!</h1>" \
-               "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'/>"
+        return too_high(guess)
+
     else:
-        return "<h1 style='color: green'>You found me!</h1>" \
-               "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'/>"
+        return correct_number(guess)
 
 
 if __name__ == "__main__":
